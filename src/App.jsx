a02232fc0c9b6d6232c05754e4b6dc8ea17ea8dd
Javokhir Fayzullaev <jofay91@gmail.com>
2024-03-14 21:58:17 +0500

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>home</title>
+        <meta name="description" content="home page is home" />
+        <link rel="canonical" href="./" />
+        <meta
+          name="google-site-verification"
+          content="n1jZsIZXvJt8nN-PBIiCJakzrWq7cflAgDhPF-a3uvg"
+        />
+      </Helmet>
       <h1>App component</h1>
       <Link to={"/login"}>go to login page</Link>
       <div></div>
